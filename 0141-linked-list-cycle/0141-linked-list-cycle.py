c8b8a92d9d62e1dc33e5542b1c58f41078175c62
lakhvindersingh17 <66323086+lakhvindersingh17@users.noreply.github.com>
2022-11-07 22:55:08 +0530

    Time: 120 ms (39.88%), Space: 17.5 MB (67.45%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -7,7 +7,7 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        if not head or not head.next:
+        if head==None:
             return False
         
         firstPoin=head
